472b85527e4d6984f3d6006c6efabc64c8f86b1b
brazzolim <124169506+brazzolim@users.noreply.github.com>
2024-12-02 19:21:01 +0100

    02-12 full ok

--- 02/0202.py
+++ 02/0202.py
@@ -7,12 +7,10 @@ reports = []
 for line in lines:
     line = line.strip()
     line = line.split()
-    
     reports.append([int(numeric_string) for numeric_string in line])
-#make array of int
-# reports = [int(i) for i in reports]
 sumSafe = 0
 safeReports = []
+unsafeReports = []
 # print(reports)
 for report in reports:
     isSafe = False
@@ -31,5 +29,44 @@ for report in reports:
                 continue
     if isSafe == True:
         safeReports.append(report)
+    else:
+        unsafeReports.append(report)
 # print(safeReports)
 print(len(safeReports))
+print(len(unsafeReports))
+
+sumSafe = 0
+# safeReports = []
+# unsafeReports = []
+for report in unsafeReports:
+    originalReport = list(report)
+    for i in range(len(report)):
+        try:
+            report.pop(i)
+            isSafe = False
+            if ((report == sorted(report, reverse = True)) or (report == sorted(report, reverse = False)) ):
+                for i in range(len(report)):
+                    try:
+                        level = report[i]
+                        nextLevel = report[i+1]
+                        diffLevels = abs(level - nextLevel)
+                        if (diffLevels <= 3 and diffLevels >= 1) :
+                            isSafe = True
+                        else:
+                            isSafe = False
+                            break
+                    except:
+                        continue
+            if isSafe == True:
+                safeReports.append(report)
+                break
+            report = list(originalReport)
+        except:
+            continue
+        
+# for a in safeReports:
+#     print(*a)
+print(len(safeReports))
+# no 255
+# no 271
+# no 1212
